a872d9bd1aa425380965f287249f5fe339f6d0ba
samncorn <samuel41@illinois.edu>
2023-06-17 06:57:13 -0700

    added csv

--- sspp.py
+++ sspp.py
@@ -26,6 +26,13 @@ from modules import PPVignetting
 #     else:
 #         print('input path suffix could not be parsed. File format may be incorrect')
 
+def checkFootprint(df, cadence, fp, recordDetector=False):
+    onSensor, detectorIDs = fp.applyFootprint(df, cadence)
+    out = df.iloc[onSensor].reset_index(drop=True, inplace=True)
+    if recordDetector:
+        out['detector'] = detectorIDs
+    return out
+
 def run(parser):
     args=parser.parse_args()
     path2cadence = args.cadence
@@ -47,12 +54,17 @@ def run(parser):
     # pplogger.info('Reading pointing database')
      # print(cadence.columns)
     # oif = loadPandas(path2input)
-    if args.h5table==None: # assumes pandas formatted hdf5 file
-        oif = pd.read_hdf(args.input).reset_index(drop=True)
-    else:
-        oif = pd.read_hdf(args.input, key=args.h5table).reset_index(drop=True)
-        # note that while we can load table from such an h5 file, they must be written to seperate output. 
-        # adds a bit of time, but not complicated to have another post-post-processing script
+    if args.format=='hdf':
+        if args.h5table==None: # assumes pandas formatted hdf5 file
+            oif = pd.read_hdf(args.input).reset_index(drop=True)
+        else:
+            oif = pd.read_hdf(args.input, key=args.h5table).reset_index(drop=True)
+            # note that while we can load table from such an h5 file, they must be written to seperate output. 
+            # adds a bit of time, but not complicated to have another post-post-processing script
+    elif args.format=='csv':
+        oif = pd.read_csv( args.inout )
+    else: # for now assume csv
+        oif = pd.read_csv( args.inout )
 
     cadencestart = oif['FieldID'].min()-1
     cadenceend= oif['FieldID'].max()+1
@@ -71,29 +83,33 @@ def run(parser):
         sys.exit('unrecognized cadence file suffix. terminating...')
     colors=pd.read_csv(path2colors, delim_whitespace=True).reset_index(drop=True)
 
+    fpPath = os.path.join(os.path.dirname(__file__), "data/detectors_corners.csv")
+    fovPath = os.path.join(os.path.dirname(__file__), "data/cameraFootprintNoGaps.csv")
+    footprint = PPFootprintFilter.Footprint(fpPath)
+    fov = PPFootprintFilter.Footprint(fovPath)
+
     # surveydb_join= pd.merge(oif["FieldID"], cadence, left_on="FieldID", right_on="observationId", how="left")
     # for name in ["fiveSigmaDepth", 'filter', 'seeingFwhmEff', 'seeingFwhmGeom']:
     #     oif[name] = surveydb_join[name]
     oif = pd.merge(oif, cadence, left_on="FieldID", right_on="observationId", how="left")
 
-    # calculate 
+    #==========================================================================
+    # do all the processing here
+
+    # drop observations not inside the camera outline
+    oif = checkFootprint(oif, cadence, fov)
+
+    # calculate apparent magnitude
     oif["Mag"]=PPTranslateMagnitude.PPTranslateMagnitude(oif, cadence, colors)
 
-    # print(oif.columns)
+    # calculate uncertainties
     oif['AstrometricSigma(mas)'], oif['PhotometricSigma(mag)'], oif["SNR"] = PPAddUncertainties.addUncertainties(oif, cadence, obsIdNameEph='FieldID', obsIdName='observationId', filterMagName='Mag')
-    # print(len(uncert[0]))
-    # print(len(oif))
     oif["AstrometricSigma(deg)"] = oif['AstrometricSigma(mas)'] / 3600 / 1000
 
     oif['dmagDetect']=PPTrailingLoss.PPTrailingLoss(oif, cadence)
     oif['dmagVignet']=PPVignetting.vignettingLosses(oif, cadence)
 
-    fp_path = os.path.join(os.path.dirname(__file__), "data/detectors_corners.csv")
-    footprint = PPFootprintFilter.Footprint(fp_path)
-    onSensor, detectorIDs = footprint.applyFootprint(oif, cadence)
-    oif=oif.iloc[onSensor]
-    oif["detectorID"] = detectorIDs
-    oif.reset_index(drop=True, inplace=True)
+    oif = checkFootprint(oif, cadence, footprint, recordDetector=True)
 
     oif.drop(columns=["AstrometricSigma(mas)"], inplace=True)
     oif['Mag_fading'] = PPRandomizeMeasurements.randomizePhotometry(oif, magName='Mag', sigName="PhotometricSigma(mag)", rng=rng)
@@ -119,6 +135,8 @@ def run(parser):
     oif.drop( np.where(oif["Mag_fading"] + oif["dmagDetect"] + oif['dmagVignet'] >= oif["fiveSigmaDepth"])[0], inplace=True)
     oif.reset_index(drop=True, inplace=True)
 
+    #==========================================================================
+
     if args.h5table==None:
         out_path_final=outpath + '.h5'
     else:
@@ -143,7 +161,7 @@ def main():
     parser.add_argument('--colors', help='path to color file', type=str)
     parser.add_argument('--save-footprint', dest='save_footprint', help='saves a copy after the footprint is run', action=argparse.BooleanOptionalAction)
     parser.add_argument('--outputstem', help='output file name', type=str, default='input')
-    # parser.add_argument('--outputformat', help='output file format', type=str)
+    parser.add_argument('--format', help='output file format', type=str, default='csv')
     parser.add_argument('--outpath', type=str, default='None')
     parser.add_argument('--h5table', type=str, default=None, help='If input file is hdf5 with multiple tables, which table to read from')
 
